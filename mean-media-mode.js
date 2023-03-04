6d1c11d8969a184ae9f87df8a30cf28242b24e4c
bhoyee <s.adeboye@outlook.com>
2023-03-04 19:13:13 +0100

    adding meanmedianmode-algorithm

---
+++ mean-media-mode.js
@@ -0,0 +1,22 @@
+ /** Mean Median Mode algorithm
+
+It algorithm will take number of array as parameter and it will return an object that have 3 properties in it which are the mean media and mode.
+
+To build this algorithm , 4 functions will be created 
+1. getMean(array), getMedian(array), getMode(array), meanMedianMode(array) ***/
+
+function meanMedianMode(array) {
+
+}
+
+function getMean(array) {
+
+}
+
+function getMedian(array) {
+
+}
+
+function getMode(array) {
+    
+}
