6ef9eb507498758eeb697602abf759cf6f93bcc4
bhoyee <s.adeboye@outlook.com>
2023-03-04 19:31:18 +0100

    update the median function

--- mean-media-mode.js
+++ mean-media-mode.js
@@ -21,18 +21,32 @@ function meanMedianMode(array) {
 function getMean(array) {
 //declear variable and set it zero
  var sum = 0;
+
  // loop thru every number in the array and add it to the sum
  array.forEach(num => {
     sum += num;
  });
  // to get the mean , we need to devide the sum by number of element in the array
   var mean = sum / array.length;
-  
+
   //return mean
   return mean;
 }
 
 function getMedian(array) {
+    //firstly sort the array 
+    array.sort(function(a, b) { return a- b});
+    var median;
+
+    if (array.length % 2 !== 0) {
+        median = array[(Math.floor(array.length / 2))];
+    }
+    else {
+        var mid1 = array[(array.length / 2) - 1]
+        var mid2 = array[array.length / 2]
+        median = (mid1 + mid2) / 2;
+    }
+    return median;
 
 }
 
