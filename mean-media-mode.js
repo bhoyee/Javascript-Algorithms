1b94dead65ada668d9be4f06a679400b0ee75b9b
bhoyee <s.adeboye@outlook.com>
2023-03-04 19:39:20 +0100

    update the mode function

--- mean-media-mode.js
+++ mean-media-mode.js
@@ -51,5 +51,27 @@ function getMedian(array) {
 }
 
 function getMode(array) {
+
+    var modeObj = {};
+
+    array.forEach(num => {
+        if (modeObj[num]) modeObj[num] = 0;
+        modeObj[num]++;
+    });
+
+    var maxFrequency = 0;
+    var modes = [];
+    for (var num in modeObj) {
+        if(modeObj[num] > maxFrequency) {
+            modes = [num];
+            maxFrequency = modeObj[num];
+        }
+        else if (modeObj[num] === maxFrequency) modes.push(num);
+    }
+    
+    if (modes.length === Object.keys(modeObj).length) modes = [];
+
+    return modes;
+
     
 }
