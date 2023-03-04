d45ea92f76d836ab83b866d70ad2a87c7049480f
bhoyee <s.adeboye@outlook.com>
2023-03-04 19:22:42 +0100

    update the mean function

--- mean-media-mode.js
+++ mean-media-mode.js
@@ -3,14 +3,33 @@
 It algorithm will take number of array as parameter and it will return an object that have 3 properties in it which are the mean media and mode.
 
 To build this algorithm , 4 functions will be created 
-1. getMean(array), getMedian(array), getMode(array), meanMedianMode(array) ***/
+ (getMean(array), getMedian(array), getMode(array), meanMedianMode(array)) ***/
 
+
+//return an object that mode mean, median , mode on it
 function meanMedianMode(array) {
+
+    return  {
+        mean: getMean(array),
+        median: getMedian(array),
+        mode: getMode(array)
+    };
 
 }
 
-function getMean(array) {
 
+function getMean(array) {
+//declear variable and set it zero
+ var sum = 0;
+ // loop thru every number in the array and add it to the sum
+ array.forEach(num => {
+    sum += num;
+ });
+ // to get the mean , we need to devide the sum by number of element in the array
+  var mean = sum / array.length;
+  
+  //return mean
+  return mean;
 }
 
 function getMedian(array) {
